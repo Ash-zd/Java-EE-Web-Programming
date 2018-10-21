97d3fd02471038f6913cdc8469e8ca3a92a73037
Ash_zd <ash_zd@qq.com>
2018-10-21 12:12:04 +0800

    exp2 5.7 done

--- exp2/src/Servlet/LoginServlet.java
+++ exp2/src/Servlet/LoginServlet.java
@@ -1,6 +1,7 @@
 package Servlet;
 
 import java.io.PrintWriter;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,7 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/Servlet/LoginServlet")
 public class LoginServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
@@ -18,10 +18,11 @@ public class LoginServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        request.setCharacterEncoding("utf-8");
+        ServletConfig config = this.getServletConfig();
         String username = request.getParameter("username");
         String password = request.getParameter("pass");
-        response.setContentType("text/html;charset=utf-8");
+        String encoding = config.getInitParameter("encoding");
+        response.setContentType("text/html;charset=" + encoding);
         PrintWriter out = response.getWriter();
         out.println("<html><head><title>登录结果</title></head>");
         out.println("<body> 您输入的用户名是：" + username + "<br>");
